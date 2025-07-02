788c4153c0809b42021511750db392944375422a
Enrique Perezalonso <epereza@umich.edu>
2025-07-02 18:27:47 +0000

    Finishing prediction mode, only missing DB now

--- mlbinfo.py
+++ mlbinfo.py
@@ -1,5 +1,16 @@
 from datetime import date, timedelta
 import statsapi
+import os 
+from google import genai
+from google.genai import types
+
+my_api_key = os.getenv('MLB_PRED')
+genai.api_key = my_api_key
+
+# Create an genAI client using the key from our environment variable
+client = genai.Client(
+    api_key=my_api_key,
+)
 
 print("Welcome to our MLB app! We have the following two functionalities:")
 print("Information Mode: We will provide valuable information on whichever team you want")
@@ -7,6 +18,7 @@ print("Good Game Predictor: You provide us a team and we will tell you which of 
 mode = input("Please input 1 for Information Mode or 2 for Good Game Predictor: ")
 
 def next_x_games(id,x):
+    s = []
     today = date.today().strftime("%Y-%m-%d")
     end = (date.today() + timedelta(days=30)).strftime("%Y-%m-%d")
     sched = statsapi.schedule(start_date=today, end_date=end, team=id)
@@ -19,7 +31,13 @@ def next_x_games(id,x):
         game_day = g['game_date']
         opponent = g['away_name'] if g['home_id'] == id else g['home_name']
         venue = g['venue_name']
-        print(f"{game_day} vs {opponent}  @ {venue}")
+        line = f"{game_day} vs {opponent}  @ {venue}"
+        print(line)
+        s.append(line)
+    return s
+    
+        
+    
 
     
 def info_mode():
@@ -57,7 +75,34 @@ def info_mode():
             print(players)
             break 
 
-#def predictor_mode():
+
+def predictor_mode():
+    team = input("What team's future do you want to know about: ")
+    while True:
+        teams = statsapi.lookup_team(team)
+        if len(teams) == 0:
+            team = input("We could not find this team, please try again: ")
+        elif len(teams) > 1:
+            print("The following teams fit under your current description, please specify which one by using name or id")
+            for t in teams:
+                print("Team name:", t['name'] , "and Team id:", t['id'])
+            team = input("Please enter either team name or id: ")
+        else:
+            t = teams[0]['id']
+            vec = next_x_games(t,5)
+            prompt = (
+                "Here are the games: \n \n" +
+                "\n".join(vec)
+            )
+            response = client.models.generate_content(
+                model="gemini-2.5-flash",
+                config=types.GenerateContentConfig(
+                system_instruction="You are a sports analyst for the MLB, you will be given 5 future games and you should give a prediction for who will win each one, You will also identify which will be the most exciting game to watch, meaning the closest of the following"
+                ),
+                contents= prompt
+            )
+            print(response.text)
+            break
 
     
 
@@ -69,6 +114,7 @@ while True:
 
     if mode == '2':
         predictor_mode()
+        break
     
     if mode == '9':
         print("You have exited")
